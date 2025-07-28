7f93f638d06739b52b74ee1593dd3a3e225b8b55
Mian <docgoosenn@gmail.com>
2025-07-28 02:26:09 -0700

    Working on DataCreator

--- src/server/main.server.ts
+++ src/server/main.server.ts
@@ -1,6 +1,11 @@
-import { Workspace } from "@rbxts/services";
+import { Players, Workspace } from "@rbxts/services";
+import { DataObject } from "shared/DataCreator";
 import { makeHello } from "shared/module";
+import { ServerDataObject } from "./ServerDataCreator";
 
-print(makeHello("main.server.ts"));
-print(Workspace.Baseplate.Name);
-print("MEOW");
+let newData = new ServerDataObject<Instance>(Workspace.WaitForChild("Baseplate"));
+
+newData.addListener("newValue", (key, value, oldValue) => print(key, value, oldValue));
+newData.replicateKeyTo("grr", ["tag", Players.WaitForChild("MianReplicate") as Player]);
+
+newData.setValue("grr", "Is that a dog?");
