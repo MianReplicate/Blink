0e8071bc36aac6a0c5f25255208f54ccb1859da1
SupBananaHere <bananaboy11309@gmail.com>
2025-07-29 19:01:54 -0400

    fuck you

--- src/server/meowmeow.server.ts
+++ src/server/meowmeow.server.ts
@@ -1,1 +1,30 @@
-let hi = 1
+// by mr sup banana here and this shit took me way to long to setup (fucking help me)
+
+class animal {
+
+    public eat() {
+
+    }
+
+    public makeSound() {
+
+    }
+
+}
+
+class dog extends animal {
+
+    public makeSound() {
+        
+    }
+
+}
+
+class cat extends animal {
+
+    public makeSound() {
+        
+    }
+
+}
+
